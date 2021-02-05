27e5027d0e6888fc82859e4dcad01d6f9be6389b
manikandan <61343429+manikandan@users.noreply.github.com>
2021-02-05 14:51:59 +0530

    feature product sum commit

--- featuredProduct.js
+++ featuredProduct.js
@@ -1,12 +1,27 @@
 function findFeatured(arr) {
-  let result = arr.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {})
-  console.log(result)
+  let result = [];
+  let map = {};
+  arr.forEach((curr) => {
+    if (curr in map) {
+      result[map[curr]].count += 1;
+    } else {
+      result.push({
+        product: curr,
+        count: 1
+      });
+      map[curr] = result.length - 1;
+    }
+  });
+  result.sort((a, b) => {
+    if (a.count === b.count) {
+      return a.product - b.product;
+    }
+    return a.count - b.count;
+  })
+  console.log(result[result.length - 1].product);
 }
 
 
-let arr = ['black', 'black', 'black', 'red', 'yellow', 'yellow', 'yellow', 'black'];
+let arr = ['black', 'black', 'black', 'red', 'yellow', 'yellow', 'yellow', 'yellow', 'black'];
 
 findFeatured(arr);
